ca4477b0e3109b325681bfe34684abad66b52a73
ssomkar <ss.omkar.23@gmail.com>
2025-11-19 15:45:31 +0530

    Create sum of largest num in the array.c

---
+++ sum of largest num in the array.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int arr[5]={1,2,3,4,5};
+    int max= arr[0];
+    for(int i=0;i<5;i++){
+        if(arr[i]>max){
+            max=arr[i];
+        }
+    }
+    printf("large element=%d\n",max);
+    return 0;
+}
