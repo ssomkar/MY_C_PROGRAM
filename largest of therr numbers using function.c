4702d34461f26af37c7193d7432f837adfb77752
ssomkar <ss.omkar.23@gmail.com>
2025-11-19 12:00:23 +0530

    Create largest of therr numbers using function.c

---
+++ largest of therr numbers using function.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int largest (int a,int b,int c){
+    if(a>=b && a>=c){
+        printf("a is largets ");
+    }else if(b>=a && b>=c){
+        printf("b is largest");
+   }else{
+        printf("c is largest");
+    }
+}
+    int main(){
+        largest(2,3,5);
+    }
