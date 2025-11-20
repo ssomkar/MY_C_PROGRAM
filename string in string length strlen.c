a0cd58c9636e8521d8cbc421f8ddb3322f078a95
ssomkar <ss.omkar.23@gmail.com>
2025-11-20 10:35:34 +0530

    Create string in string length strlen.c

---
+++ string in string length strlen.c
@@ -0,0 +1,9 @@
+ #include<stdio.h>
+    #include<string.h>
+    int main(){
+        char rajesh[20];
+        printf("enter  string array\n");
+        fgets(rajesh,sizeof(rajesh),stdin);
+        int c = strlen("rose kannan");
+        printf("%d",c);
+    }
