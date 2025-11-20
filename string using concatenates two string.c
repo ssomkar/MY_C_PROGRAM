eadb9805326c778a5cf06a26efc994bfb46e7fa3
ssomkar <ss.omkar.23@gmail.com>
2025-11-20 10:53:50 +0530

    Create string using concatenates two string.c

---
+++ string using concatenates two string.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+    #include<string.h>
+    int main(){
+        char pushpa[10];
+        printf("enter your string\n");
+        scanf("%s\n",&pushpa);
+    strcat (pushpa,"flower");
+        printf("%s\n",pushpa);
+        strcat (pushpa,"fire");
+        printf("%s",pushpa);
+    }
