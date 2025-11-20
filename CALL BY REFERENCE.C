5549581175fbe419568753288c2e1c7732656f1b
ssomkar <ss.omkar.23@gmail.com>
2025-11-20 13:02:58 +0530

    Create CALL BY REFERENCE.C

---
+++ CALL BY REFERENCE.C
@@ -0,0 +1,12 @@
+  #include<stdio.h>
+  int main(){
+      int a=10,b=20;
+      int temp,*p1,*p2;
+      p1=&a;
+      p2=&b;
+      printf("%d %d\n",a,b);
+      temp=*p1;
+      *p1=*p2;
+      *p2=temp;
+      printf("%d %d\n",a,b);
+  }
